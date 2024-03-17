8b50faee44e640dd39ed1d573bcfe518aa7afee2
muhammad <mo.alidadi@gmail.com>
2024-03-17 06:44:56 -0700

    make samples in a array

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { ResumeSlider } from "./ResumeSlider/ResumeSlider";
 import backgroundImage from '../assets/svg-bg.svg'
-
+import { Samples } from "./Samples/Samples";
 function Home (){
 
 
@@ -24,48 +24,12 @@ function Home (){
     
     <ResumeSlider />
 
+    <Samples />
 
 
 
-    <section id="ex-pro">
-        <div class="center-divMain mr-4 text-3xl w-fit p-3 m-1">
-            <h1>نمونه کارها:</h1>
-        </div>
 
-        <div class="md:flex flex-wrap justify-center mx-3 md:mx-10 border border-slate-500 rounded-lg">
-            <div class="md:w-[525px] mt-6 md:mx-6 cursor-pointer">
-                <img src="./images/portfilo.png" alt="" srcset="" class="rounded-lg border border-slate-500  " />
-                <div class="center-divMain text-2xl p-2 w-fit -m-4 hover:scale-125 ease-in duration-300" >
-                    <h1>سایت پورتفیلو: tailwind, swipper, React</h1>
-                </div>
-            </div>
-
-            <div class="md:w-[525px] mt-6 md:mx-6">
-                <img src="./images/portfilo.png" alt="" srcset="" class="rounded-lg border border-slate-500 " />
-                <div class="center-divMain text-2xl p-2 w-fit -m-4 hover:scale-125 ease-in duration-300 " >
-                    <h1>سایت پورتفیلو</h1>
-                </div>
-            </div>
-
-            <div class="md:w-[525px] mt-6 md:mx-6">
-                <img src="./images/portfilo.png" alt="" srcset="" class="rounded-lg border border-slate-500 " />
-                <div class="center-divMain text-2xl p-2 w-fit -m-4 hover:scale-125 ease-in duration-300" >
-                    <h1>سایت پورتفیلو</h1>
-                </div>
-            </div>
-
-            <div class="md:w-[525px] mt-6 md:mx-6">
-                <img src="./images/portfilo.png" alt="" srcset="" class="rounded-lg border border-slate-500 " />
-                <div class="center-divMain text-2xl p-2 w-fit -m-4 hover:scale-125 ease-in duration-300 " >
-                    <h1>سایت پورتفیلو</h1>
-                </div>
-            </div>
-
-        
-        </div>
-
-
-    </section>
+    
 
 
     <section id="aboutme" class="mt-9 flex flex-col items-center md:flex-row md:justify-end md:items-start">
--- src/components/Samples/Samples.jsx
+++ src/components/Samples/Samples.jsx
@@ -1,1 +1,45 @@
 
+
+export const Samples = ()=>{
+
+    const sampleArrays = ["سایت پورتفیلو: tailwind, swipper, React", "محاسبه pH", "مثال لیست", "مثال"]
+
+    
+    return (
+        <section>
+        <div class="center-divMain mr-4 text-3xl w-fit p-3 m-1">
+            <h1>نمونه کارها:</h1>
+        </div>
+
+        <div class="md:flex flex-wrap justify-center mx-3 md:mx-10 border border-slate-500 rounded-lg">
+
+                    {
+                        sampleArrays.map((samp)=>{
+                            return(
+                                <div class="md:w-[525px] mt-6 md:mx-6 cursor-pointer">
+                                    <div  alt="" srcset="" class="rounded-lg border border-slate-500" ></div>
+                                        <div class="center-divMain text-2xl p-2 w-fit -m-4 hover:scale-125 ease-in duration-300" >
+                                            <h1>{samp}</h1>
+                
+                </div>
+            </div>
+                            )
+                        })
+                    }
+
+
+            
+
+            
+
+            
+
+            
+
+        
+        </div>
+
+
+    </section>
+    )
+}
